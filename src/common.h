cb74d2157c0f1d5cef3fe9f9a71cee0b48fdb086
Srikanth MADIKERI <srikanth.madikeri@idiap.ch>
2020-08-18 16:00:03 +0200

    adding nnet3/nnet-normalize-component.h

--- src/common.h
+++ src/common.h
@@ -14,6 +14,7 @@
 #include "nnet3/am-nnet-simple.h"
 #include "nnet3/nnet-utils.h"
 #include "nnet3/nnet-simple-component.h"
+#include "nnet3/nnet-normalize-component.h"
 #include "cudamatrix/cu-matrix.h"
 
 // Pytorch imports
