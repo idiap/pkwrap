892d82f2fe986ee452e85f6de68ec9c443ba5774
Srikanth MADIKERI <srikanth.madikeri@idiap.ch>
2020-08-18 12:37:41 +0200

    added functions to load and save NNET3 TDNN models

--- README.md
+++ README.md
@@ -1,53 +1,7 @@
 # README
 
-This is a (yet another!) python wrapper for Kaldi. The main goal is to be able to train acoustic models in Pytorch
-so that we can
+BRANCH-INFO: This branch contains code to load models trained in Kaldi. Currently only TDNN model is supported. This includes support for the following layers.
 
-1. use MMI cost function during training 
-2. use NG-SGD for affine transformations, which enables multi-GPU training with SGE
-
-## MOTIVATION 
-
-The main motivation of this project is to run MMI training in Pytorch. The idea is to use existing 
-functionality in Kaldi so that we don't have to re-implement anything. 
-
-### Why not use the existing wrappers?
-
-**Pykaldi** is good for exposing high-level functionalties. However, many things are still not possible (e.g.
-loading NNet3 models and having access to the parameters of the model). Modifying Pykaldi requires us to have
-custom versions of Kaldi and CLIF. Moreover, if one simply wanted to converting Tensors in GPU to Kaldi CuMatrix
-was not efficient (the general route afaik would be Tensor GPU -> Tensor CPU -> Kaldi Matrix CPU -> Kaldi Matrix GPU).
-
-**Pykaldi2** provides a version of LF-MMI training, which uses Pykaldi functions.
-
-## INSTALLATION
-
-**ATTENTION!!!**: Make sure that your pytorch environment is loaded before following the rest of the instructions.
-
-1. Activate your pytorch environment
-2. Compile Kaldi with `CXXFLAGS="-D_GLIBCXX_USE_CXX11_ABI=0"`. This code has been tested to work with Kaldi ``6f329a62``.
-3. Set ``KALDI_ROOT`` and optionally ``MKL_ROOT`` in the environment. Note: in the future this will be made easier with autoconf.
-4. Run ``make``
-
-### KNOWN ISSUES/COMMON PITFALLS
-
-- the g++ version of pytorch, kaldi and pkwrap should match!
-
-## USAGE
-
-
-Before importing do check if Kaldi libraries used to compile the package are accessible by python.
-Otherwise, it should be added to ``$LD_LIBRARY_PATH`` as follows
-
-```
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$KALDI_ROOT/src/lib
-```
-
-Currently there is one functional recipe for the conventional LF-MMI training with 
-pkwrap
-
-- [Minilibrespeech recipe](egs/mini_librespeech/s5/README.md)
-
-## REFERENCES & CITATION
-
-Technical report coming soon!
+1. Affine transforms
+2. Batchnorm
+3. ReLU
--- src/nnet3.cc
+++ src/nnet3.cc
@@ -3,6 +3,14 @@
 
 #include "nnet3.h"
 
+// from https://stackoverflow.com/questions/874134/find-out-if-string-ends-with-another-string-in-c
+// Under Creative Commons License
+inline bool ends_with(std::string const & value, std::string const & ending)
+{
+        if (ending.size() > value.size()) return false;
+        return std::equal(ending.rbegin(), ending.rend(), value.rbegin());
+}
+
 // A wrapper to PreconditionDirections of OnlineNaturalGradient: it gets a pointer
 // to the Tensor in the GPU and passed it to PreconditionDirections
 kaldi::BaseFloat precondition_directions(kaldi::nnet3::OnlineNaturalGradient &state, torch::Tensor &grad) {
@@ -10,4 +18,105 @@ kaldi::BaseFloat precondition_directions(kaldi::nnet3::OnlineNaturalGradient &st
     auto grad_cumat = TensorToKaldiCuSubMatrix(grad);
     state.PreconditionDirections(&grad_cumat, &scale_);
     return scale_;
+}
+
+std::vector<std::pair<std::string,std::vector<torch::Tensor> > > GetNNet3Components(std::string model_path) {
+    using namespace kaldi;
+    using namespace kaldi::nnet3;
+    typedef kaldi::int32 int32;
+    kaldi::nnet3::Nnet nnet;
+    ReadKaldiObject(model_path, &nnet);
+    auto nc = nnet.NumComponents();
+    std::vector<std::pair<std::string, std::vector<torch::Tensor> > > model;
+    for(int32 i=0; i<nc; i++) {
+        auto c = nnet.GetComponent(i);
+        auto name = nnet.GetComponentName(i);
+        if(ends_with(name, "affine")) {
+            kaldi::nnet3::NaturalGradientAffineComponent* cg = (kaldi::nnet3::NaturalGradientAffineComponent*) c;
+            torch::Tensor t_lp = KaldiCudaMatrixToTensor(cg->LinearParams()).clone().detach();
+            torch::Tensor t_bp = KaldiCudaVectorToTensor(cg->BiasParams()).clone().detach();
+            std::vector<torch::Tensor> params;
+            params.push_back(t_lp);
+            params.push_back(t_bp);
+            model.push_back(std::make_pair(name, params));
+        }
+        else if(ends_with(name, "batchnorm")) {
+            kaldi::nnet3::BatchNormComponent* cg = (kaldi::nnet3::BatchNormComponent*) c;
+            cg->SetTestMode(true);
+            torch::Tensor scale = KaldiCudaVectorToTensor(cg->Scale()).clone().detach();
+            torch::Tensor offset = KaldiCudaVectorToTensor(cg->Offset()).clone().detach();
+            std::vector<torch::Tensor> params;
+            params.push_back(scale);
+            params.push_back(offset);
+            model.push_back(std::make_pair(name, params));
+        }
+        else if(ends_with(name, "lda")) {
+            kaldi::nnet3::FixedAffineComponent* cg = (kaldi::nnet3::FixedAffineComponent*) c;
+            torch::Tensor t_lp = KaldiCudaMatrixToTensor(cg->LinearParams()).clone().detach();
+            torch::Tensor t_bp = KaldiCudaVectorToTensor(cg->BiasParams()).clone().detach();
+            std::vector<torch::Tensor> params;
+            params.push_back(t_lp);
+            params.push_back(t_bp);
+            model.push_back(std::make_pair(name, params));
+        }
+    }
+    return model;
+}
+
+void SaveNNet3Components(std::string model_path,
+                         std::string new_model_path,
+                         std::vector<std::pair<std::string,std::vector<torch::Tensor> > > & new_params) {
+    using namespace kaldi;
+    using namespace kaldi::nnet3;
+    typedef kaldi::int32 int32;
+    kaldi::nnet3::Nnet nnet;
+    ReadKaldiObject(model_path, &nnet);
+    auto nc = nnet.NumComponents();
+    int32 j=1;
+    // TODO: change j accordingly when batchnorm component is also quantized
+
+    for(int32 i=0; i<nc; i++) {
+        auto c = nnet.GetComponent(i);
+        auto name = nnet.GetComponentName(i);
+        if (name =="prefinal-xent.affine") {
+            break;
+        }
+        if(ends_with(name, "affine")  ) {
+            auto c_params = new_params[j];
+            if(c_params.first.compare(name)!=0) {
+                std::cout << "ERROR: expected " << name << " but found " << c_params.first << std::endl;
+                return;
+            }
+            torch::Tensor lp_float32 = c_params.second[0];
+            torch::Tensor bp_float32 = c_params.second[1];
+            // kaldi::CuSubMatrix<kaldi::BaseFloat> lp_kc = TensorToKaldiCuSubMatrix(lp_float32);
+            // kaldi::CuSubVector<kaldi::BaseFloat> bp_kc = TensorToKaldiCuSubVector(bp_float32);
+
+
+            auto nr_lp = lp_float32.size(0), nc_lp = lp_float32.size(1),
+                  nr_bp = bp_float32.size(0), nc_bp = bp_float32.size(1);
+            torch::Tensor new_params = torch::zeros({nr_lp*nc_lp+nr_bp*nc_bp,1});
+            new_params.narrow(0, 0, nr_lp*nc_lp).copy_(lp_float32.reshape({nr_lp*nc_lp,1}));
+            new_params.narrow(0, nr_lp*nc_lp, nr_bp).copy_(bp_float32.reshape({nr_bp,1}));
+            kaldi::Vector<kaldi::BaseFloat> vec(static_cast<int32>(new_params.size(0)));
+            TensorToKaldiVector(new_params, vec);
+            kaldi::nnet3::NaturalGradientAffineComponent* cg = (kaldi::nnet3::NaturalGradientAffineComponent*) c;
+            //std::cout << cg->NumParameters() << "  " << vec.Dim() << std::endl;
+            //std::cout << nr_lp << " " << nc_lp << " " << nr_bp << " " << nc_bp << std::endl;
+            //std::cout << cg->InputDim() << " " << cg->OutputDim() << std::endl;
+            cg->UnVectorize(vec);
+            j+=2;
+        }
+    }
+    WriteKaldiObject(nnet, new_model_path, true);
+    std::cout <<"Succesfully wrote model!" << std::endl;
+}
+
+torch::Tensor LoadAffineTransform(std::string matrix_path) {
+    using namespace kaldi;
+    using namespace kaldi::nnet3;
+    typedef kaldi::int32 int32;
+    kaldi::Matrix<kaldi::BaseFloat> mat;
+    ReadKaldiObject(matrix_path, &mat);
+    return KaldiMatrixToTensor(mat);
 }
