b693525e4ff3caca13ecf733c60395f2ef6a73f2
Srikanth MADIKERI <srikanth.madikeri@idiap.ch>
2020-12-13 11:39:06 +0100

    local/chain/tuning/tdnnf_17l.py: adding dropout

--- egs/librispeech/v1/local/chain/tuning/tdnnf_17l.py
+++ egs/librispeech/v1/local/chain/tuning/tdnnf_17l.py
@@ -24,32 +24,42 @@ class Net(nn.Module):
         super(Net, self).__init__()
 
         def get_tdnnf_layer(input_dim, layer_dim):
-            return TDNNFBatchNorm(
+            return nn.Sequential(
+                TDNNFBatchNorm(
                 input_dim, 
                 layer_dim, 
                 context_len=3, 
                 orthonormal_constraint=-1.0, 
                 bypass_scale=0.75,
                 bottleneck_dim=160,
-            )
+                ),
+                nn.Dropout(0.1)
+        )
         
         self.tdnnf_layers = nn.Sequential(
                     TDNNFBatchNorm(feat_dim, 1536, context_len=3, orthonormal_constraint=-1.0, bottleneck_dim=160),
+                    nn.Dropout(0.1),
                     *[get_tdnnf_layer(1536, 1536) for i in range(2, 4)],
                     TDNNFBatchNorm(1536, 1536, context_len=3, subsampling_factor=3, orthonormal_constraint=-1.0, bottleneck_dim=160),
+                    nn.Dropout(0.1),
                     *[get_tdnnf_layer(1536, 1536) for i in range(5, 18)],
                     OrthonormalLinear(1536, 256, scale=-1.0),
+                    nn.Dropout(0.1),
         )
         self.chain_layers = nn.Sequential(
                 OrthonormalLinear(256, 1536, scale=-1.0),
+                nn.Dropout(0.1),
                 OrthonormalLinear(1536, 256, scale=-1.0),
+                nn.Dropout(0.1),
                 NaturalAffineTransform(256, output_dim),
         )
         self.chain_layers[-1].weight.data.zero_()
         self.chain_layers[-1].bias.data.zero_()
         self.xent_layers = nn.Sequential(
                 OrthonormalLinear(256, 1536, scale=-1.0),
+                nn.Dropout(0.1),
                 OrthonormalLinear(1536, 256, scale=-1.0),
+                nn.Dropout(0.1),
                 NaturalAffineTransform(256, output_dim),
         )
         self.xent_layers[-1].weight.data.zero_()
