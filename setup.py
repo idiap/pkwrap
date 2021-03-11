6a333a9bcad444a4c9a906ca90f61bca02574a7d
Srikanth MADIKERI <srikanth.madikeri@idiap.ch>
2021-03-11 11:15:10 +0100

    version bump, compiling fst.cc as well

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ SRC_FILES = ['src/pkwrap-main.cc',
              'src/matrix.cc',
              'src/chain.cc',
              'src/nnet3.cc',
+             'src/fst.cc',
             ]
 EXTRA_COMPILE_ARGS = {
     'cxx':[ '-I{}/src'.format(KALDI_ROOT),
@@ -66,7 +67,7 @@ with open('./AUTHORS') as ipf:
     AUTHOR_STR = ','.join(AUTHORS)
 
 LICENSE = 'Apache 2.0'
-VERSION = '0.2.30'
+VERSION = '0.2.30.7'
 
 setup(name=PACKAGE_NAME,
       version=VERSION,
