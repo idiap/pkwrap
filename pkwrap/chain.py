d61432a3405cf046915b18e6020ded9f6f77f1dc
Srikanth MADIKERI <srikanth.madikeri@idiap.ch>
2021-03-11 11:14:04 +0100

    pkwrap/chain.py: adding EgsInfo and related classes. mainly commiting before merging from master

--- pkwrap/chain.py
+++ pkwrap/chain.py
@@ -8,6 +8,7 @@ from collections import OrderedDict, Counter
 import logging
 import argparse
 from dataclasses import dataclass
+from librosa.core.constantq import __num_two_factors
 import torch
 import torch.nn as nn
 from torch.nn.utils import clip_grad_value_
@@ -17,6 +18,10 @@ from . import chain
 from . import matrix
 from . import script_utils
 from collections import defaultdict, namedtuple
+import librosa
+import subprocess
+import io
+from math import ceil
 
 class KaldiChainObjfFunction(torch.autograd.Function):
     """LF-MMI objective function for pytorch
@@ -799,47 +804,13 @@ class ChainE2EModel(ChainModel):
         self.save_context(0)
 
 
-EgsInfo = namedtuple('EgsInfo', ['name', 'wav', 'fst', 'duration'])
-def read_wav_scp(wav_scp):
-    utt2wav = {}
-    with open(wav_scp) as ipf:
-        for ln in ipf:
-            lns = ln.strip().rstrip('|').split()
-            uttname = lns[0]
-            utt2wav[uttname] = lns[1:]
-    return utt2wav
-
-def read_utt2dur_file(utt2dur_file):
-    utt2dur = {}
-    with open(utt2dur_file) as utt2dur_f:
-        for ln in utt2dur_f:
-            lns = ln.strip().split()
-            utt2dur[lns[0]] = float(lns[1])
-    return utt2dur
-
-def get_egs_holder(fst_file, utt2wav, utt2dur):
-    egs_holder = []
-    with open(fst_file) as ipf:
-        total, done, skipped = 0, 0, 0
-        for ln in ipf:
-            lns = ln.strip().split()
-            total += 1
-            try:
-                uttname, fstscp = lns
-            except:
-                logging.erorr("Excepted fst file {} to have only 2 columns".format(fst_file))
-            if uttname not in utt2wav:
-                skipped += 1
-                continue
-            if uttname not in utt2dur:
-                logging.warning("Cannot find duration for {}".format(uttname))
-                skipped += 1
-                continue
-            this_egs_info = EgsInfo(uttname, utt2wav[uttname], fstscp, utt2dur[uttname])
-            egs_holder.append(this_egs_info)
-            done += 1
-        logging.info("In get_egs_holder: total={}, done={}, skipped={}".format(total, done, skipped))
-    return egs_holder
+@dataclass
+class EgsInfo:
+    name: str = ''
+    wav: str = ''
+    fst: str = ''
+    duration: float = 0.
+    num_output_frames: int = 0
 
 def prepare_e2e_minibatch():
     pass
@@ -858,22 +829,126 @@ class BatchSampler(torch.utils.data.BatchSampler):
                 yield prepare_e2e_minibatch(batch_by_length[l])
 
 class E2EEgsDataset(torch.utils.data.Dataset):
-    def __init__(self, egs_folder, cegs_idx):
-        self.egs_holder = self.prepare_egs(egs_folder, cegs_idx)
+    def __init__(self, egs_folder, cegs_idx, transition_model, normalization_fst, sampling_rate=16000):
+        """instantiates a Pytorch Dataset for E2E training
+
+        Args:
+            egs_folder: the folder containing egs
+            cegs_idx: index of egs, s.t. egs.1.scp exists
+            transition_model: transition model that maps transition ids to pdf ids
+            normalization_fst: fst to normalize when supervision is created
+            sampling_rate: sampling rate of the audio files
+        """
+        self.transition_model = transition_model
+        self.normalization_fst = normalization_fst
+        self.sampling_rate = sampling_rate
+        self.prepare_egs(egs_folder, cegs_idx)
 
     def __len__(self):
         return len(self.egs_holder)
 
+    # TODO: test this function
+    def process_egs(self, egs):
+        min_diff = 100
+        len_extend_context = 0
+        # first read the wavfile
+        try:
+            p = subprocess.Popen(egs.wav, stdout=subprocess.PIPE)
+            samples, sampling_rate = librosa.load(io.BytesIO(p.communicate()[0]), sr=self.sampling_rate)
+            samples = samples.reshape(1, -1)
+        except:
+            raise IOError("Error processing {}".format(egs.name))
+        duration = samples.shape[1]/sampling_rate
+        if duration not in self.allowed_durations:
+            raise ValueError("Cannot find duration for {}".format(egs.name))
+        num_samples = samples.shape[1]
+        num_output_frames = egs.num_output_frames
+        fst = kaldi.fst.StdVectorFst()
+        kaldi.fst.ReadKaldiFst(egs.fst, fst)
+        supervision = kaldi.chain.Supervision()
+        # TODO: make sure transition model is loaded
+        if not kaldi.chain.TrainingGraphToSupervisionE2e(fst, self.trans_mdl, num_output_frames, supervision):
+            raise Exception("Cannot convert fst to supervision for {}".format(egs.name))
+        # TODO: add num_states function to Fst
+        if self.normalization_fst is not None and self.normalization_fst.num_states() > 0:
+            if not kaldi.chain.AddWeightToSupervisionFst(self.normalization_fst, supervision):
+                logging.warning("FST was empty for utterance {}".format(egs.name))
+        return supervision
+
+
     def __item__(self, i):
         # we may need to return wav and normalized fst instead
-        return self.egs_holder[i]
+        return self.process_egs(self.egs_holder[i])
 
-    def prepare_egs(egs_folder, cegs_index):
+    # TODO: test this function
+    def prepare_egs(self, egs_folder, cegs_index):
         # egs file is wav.scp file
         egs_file = "{}/cegs.{}.ark".format(egs_folder, cegs_index)
         fst_file = "{}/fst.{}.scp".format(egs_folder, cegs_index)
         utt2dur_file = "{}/utt2dur.{}".format(egs_folder, cegs_index)
-        utt2wav = read_wav_scp(egs_file)
-        utt2dur = read_utt2dur_file(utt2dur_file)
-        egs_holder = get_egs_holder(fst_file, utt2wav, utt2dur)
+        utt2len_file = "{}/utt2len.{}".format(egs_folder, cegs_index)
+        utt2wav = self._read_wav_scp(egs_file)
+        utt2dur = self._read_utt2dur_file(utt2dur_file)
+        utt2len = self._read_utt2len_file(utt2len_file)
+        egs_holder = self._get_egs_holder(fst_file, utt2wav, utt2dur, utt2len)
+
+        allowed_durations_filename = "{}/allowed_durations.txt".format(egs_folder)
+        self.allowed_durations = set([float(ln) for ln in open(allowed_durations_filename)])
+        self.allowed_durations_sorted = sorted(list(self.allowed_durations))
+        self.egs_holder = egs_holder
+
+    def _read_wav_scp(self, wav_scp):
+        utt2wav = {}
+        with open(wav_scp) as ipf:
+            for ln in ipf:
+                lns = ln.strip().rstrip('|').split()
+                uttname = lns[0]
+                utt2wav[uttname] = lns[1:]
+        return utt2wav
+
+    # TODO: refactor this into some utilility that can read 2 column files
+    def _read_utt2dur_file(self, utt2dur_file):
+        """read utt2dur file"""
+        utt2dur = {}
+        with open(utt2dur_file) as utt2dur_f:
+            for ln in utt2dur_f:
+                lns = ln.strip().split()
+                utt2dur[lns[0]] = float(lns[1])
+        return utt2dur
+
+    def _read_utt2len_file(self, utt2len_file):
+        """read utt2len file, second column is the number of output frames"""
+        utt2len = {}
+        with open(utt2len_file) as utt2len_f:
+            for ln in utt2len_f:
+                lns = ln.strip().split()
+                utt2len[lns[0]] = float(lns[1])
+        return utt2len
+
+    def _get_egs_holder(self, fst_file, utt2wav, utt2dur, utt2len):
+        egs_holder = []
+        with open(fst_file) as ipf:
+            total, done, skipped = 0, 0, 0
+            for ln in ipf:
+                lns = ln.strip().split()
+                total += 1
+                try:
+                    uttname, fstscp = lns
+                except:
+                    logging.error("Excepted fst file {} to have only 2 columns".format(fst_file))
+                if uttname not in utt2wav:
+                    skipped += 1
+                    continue
+                if uttname not in utt2dur:
+                    logging.warning("Cannot find duration for {}".format(uttname))
+                    skipped += 1
+                    continue
+                if uttname not in utt2len:
+                    logging.warning("Cannot find number of output frames for {}".format(uttname))
+                    skipped += 1
+                    continue
+                this_egs_info = EgsInfo(uttname, utt2wav[uttname], fstscp, utt2dur[uttname], utt2len[uttname])
+                egs_holder.append(this_egs_info)
+                done += 1
+            logging.info("In get_egs_holder: total={}, done={}, skipped={}".format(total, done, skipped))
         return egs_holder
