f4e379d13d405a699035e31329abd201df8624cd
Srikanth MADIKERI <srikanth.madikeri@idiap.ch>
2021-04-17 12:37:48 +0200

    added shell option to run function

--- pkwrap/script_utils.py
+++ pkwrap/script_utils.py
@@ -223,9 +223,9 @@ def feat_writer(feature_wspec):
     writer = kaldi.matrix.BaseFloatMatrixWriter(feature_wspec)
     return writer
 
-def run(cmd, quit_on_error=True):
+def run(cmd, quit_on_error=True, shell=False):
     """Run a command using subprocess, quit if return code is non-zero"""
-    p = subprocess.run(cmd)
+    p = subprocess.run(cmd, shell=shell)
     if quit_on_error and p.returncode != 0:
         quit(p.returncode)
 
