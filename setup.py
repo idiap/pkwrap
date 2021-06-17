ad5283eb405d12f100b58f27c31a3a98ab08c97d
Srikanth MADIKERI <srikanth.madikeri@idiap.ch>
2021-06-17 16:34:32 +0200

    formatting lists

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 # Copyright (c) 2020 Idiap Research Institute, http://www.idiap.ch/
 #  Written by Srikanth Madikeri <srikanth.madikeri@idiap.ch>
 
-# Welcome to pkwrap 
-# This tool allows to train acoustic models with Pytorch using Kaldi's LF-MMI cost
-# function
+# Welcome to pkwrap
+# This tool allows to train acoustic models with Pytorch using Kaldi's LF-MMI
+# cost function.
 # Run python setup.py install to build and install the library.
 
 import os
@@ -12,9 +12,9 @@ from setuptools import setup, find_packages
 from torch.utils import cpp_extension
 
 import torch
-pytorch_version = torch.__version__
-pytorch_major_ver, pytorch_min_ver = list(map(int, pytorch_version.split('.')[:2]))
-if pytorch_major_ver != 1 or pytorch_min_ver > 8:
+PYTORCH_VERSION = torch.__version__
+PYTORCH_MAJOR_VER, PYTORCH_MIN_VER = list(map(int, PYTORCH_VERSION.split('.')[:2]))
+if PYTORCH_MAJOR_VER != 1 or PYTORCH_MIN_VER > 8:
     sys.stderr.write("We support pytorch version until 1.8 only\n")
     quit(1)
 
@@ -26,27 +26,30 @@ KALDI_LIB_DIR = os.path.join(KALDI_ROOT, 'src', 'lib')
 
 PACKAGE_NAME = 'pkwrap'
 EXTENSION_NAME = '_pkwrap'
-SRC_FILES = ['src/pkwrap-main.cc',
-             'src/matrix.cc',
-             'src/chain.cc',
-             'src/nnet3.cc',
-            ]
+SRC_FILES = [
+    'src/pkwrap-main.cc',
+    'src/matrix.cc',
+    'src/chain.cc',
+    'src/nnet3.cc',
+]
 EXTRA_COMPILE_ARGS = {
-    'cxx':[ '-I{}/src'.format(KALDI_ROOT),
-            '-I{}/tools/openfst/include'.format(KALDI_ROOT),
-            '-m64',
-            '-msse',
-            '-msse2',
-            '-DHAVE_CUDA=1',
-            '-Wno-sign-compare', # this helped clear openfst related compilation errors
-            # additional flags used by Kaldi, but not sure we need this
-            '-Wno-deprecated-declarations',
-            '-Winit-self',
-            '-DKALDI_DOUBLEPRECISION=0',
-            '-DHAVE_EXECINFO_H=1',
-            '-w', # potentially dangerous, but less annoying
-          ]
-    }
+    'cxx': [
+        '-I{}/src'.format(KALDI_ROOT),
+        '-I{}/tools/openfst/include'.format(KALDI_ROOT),
+        '-m64',
+        '-msse',
+        '-msse2',
+        '-DHAVE_CUDA=1',
+        # this helped clear openfst related compilation errors
+        '-Wno-sign-compare',
+        # additional flags used by Kaldi, but not sure we need this
+        '-Wno-deprecated-declarations',
+        '-Winit-self',
+        '-DKALDI_DOUBLEPRECISION=0',
+        '-DHAVE_EXECINFO_H=1',
+        '-w',  # potentially dangerous, but less annoying
+    ]
+}
 LIBRARIES = [
     "kaldi-base", "kaldi-matrix", "kaldi-util", "kaldi-cudamatrix",
     "kaldi-decoder", "kaldi-lat", "kaldi-gmm", "kaldi-hmm", "kaldi-tree",
@@ -72,12 +75,12 @@ setup(name=PACKAGE_NAME,
       version=VERSION,
       author=AUTHOR_STR,
       license=LICENSE,
-      packages = find_packages(),
-      ext_modules=[cpp_extension.CppExtension(EXTENSION_NAME, SRC_FILES,
-                                              language="c++",
-                                              extra_compile_args=EXTRA_COMPILE_ARGS,
-                                              libraries=LIBRARIES,
-                                              library_dirs=LIBRARY_DIRS,
-                                             )
-                  ],
+      packages=find_packages(),
+      ext_modules=[cpp_extension.CppExtension(
+          EXTENSION_NAME, SRC_FILES,
+          language="c++",
+          extra_compile_args=EXTRA_COMPILE_ARGS,
+          libraries=LIBRARIES,
+          library_dirs=LIBRARY_DIRS,
+      )],
       cmdclass={'build_ext': cpp_extension.BuildExtension})
